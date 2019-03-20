f10b54f0b7eb63ab4900e29521053ce60ebd10a7
pengfeiXV <877107913@qq.com>
2019-03-20 23:17:37 +0800

    arrange

--- app/serializer/arrange.py
+++ app/serializer/arrange.py
@@ -3,5 +3,5 @@ from marshmallow import Schema, fields
 
 
 class ArrangeParaSchema(Schema):
-    user_id = fields.Steing(required=True)
+    user_id = fields.String(required=True)
     free_time = fields.Integer(required=True, validate=lambda x: 0 < x <= 2 ** 24)
