8457275d0b8739c1ff48d2483581fe3b36a5e88d
pengfeiXV <877107913@qq.com>
2019-03-22 10:11:05 +0800

    admin/info

--- app/view/admin/info.py
+++ app/view/admin/info.py
@@ -1,26 +1,44 @@
-#  coding: utf-8
+# coding: utf-8
 
-from flask_login import login_required, current_user
-from flask import request, Blueprint
-from app.serializer.info import InfoParaSchema, InfoSearchSchema
-from app.libs.http import error_jsonify, jsonify
-from app.const.user import PERMISSION_TEACHER
+from flask import Blueprint, request
+from flask_login import login_required
+from app.model.info import Info
+from app.decorator.auth import admin_required
+from app.libs.http import jsonify, error_jsonify
+from sqlalchemy import and_
+from app.const.errors import NoStudentInfo
 
-# 数据汇总
-bp_admin_info = Blueprint('admin_info', __name__, url_prefix='/admin/info')
+bp_account = Blueprint('info', __name__, url_prefix='/info')
 
 
-@bp_admin_info.route("/", methods=["POST"])
+@bp_account.route('/', methods=['GET'])
 @login_required
 @admin_required
-def get_info():
-    json = request.get_json()
-    data, errors = InfoSearchSchema().load(json)
-    if errors:
-        return error_jsonify(10000001, errors)
-    area = u'%{}%'.format(data.get('area'))
-    infos = Info.query.filter(Info.area.like(area))
-    if current_user.isAdmin == PERMISSION_CITY:
-        infos.filter(Info.area.like(u'%{}%'.format(current_user.area)))
-    json = InfoParaSchema(many=True).dump(infos.all())
-    return jsonify(json.data)
+def info():
+    # 管理员排班审批
+
+    department_id = request.args.get('department_id')
+    position_id = request.args.get('position_id')
+    time = request.args.get('time')
+
+    time_list = [i for i in range(len(time)) if time[i] == '1']  # 提取所有有空的时间段
+
+    info = Info.query.filter(and_(
+        Info.department_id == department_id,
+        Info.position_id == position_id,
+        Info.free_time & time != 0
+    )).all()
+
+    if info is None:
+        return error_jsonify(NoStudentInfo)
+
+    students = []
+    for i in info:
+        student = {
+            'name':i.name,
+            'sex':i.sex,
+            'student_id':i.student_id
+        }
+        students.append(student)
+
+    return jsonify(info)
