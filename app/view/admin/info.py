1da93b1582e91eb32a385d237c1a648531c2a8ec
pengfeiXV <877107913@qq.com>
2019-03-22 10:13:15 +0800

    admin/info 修改部分注释

--- app/view/admin/info.py
+++ app/view/admin/info.py
@@ -15,13 +15,11 @@ bp_account = Blueprint('info', __name__, url_prefix='/info')
 @login_required
 @admin_required
 def info():
-    # 管理员排班审批
+    # 管理员查看排版系统
 
     department_id = request.args.get('department_id')
     position_id = request.args.get('position_id')
     time = request.args.get('time')
-
-    time_list = [i for i in range(len(time)) if time[i] == '1']  # 提取所有有空的时间段
 
     info = Info.query.filter(and_(
         Info.department_id == department_id,
@@ -35,10 +33,10 @@ def info():
     students = []
     for i in info:
         student = {
-            'name':i.name,
-            'sex':i.sex,
-            'student_id':i.student_id
-        }
+            'name': i.name,
+            'sex': i.sex,
+            'student_id': i.student_id
+        }  # 学生简略信息
         students.append(student)
 
     return jsonify(info)
