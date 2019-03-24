a94f25c19670f94abf50a11644d9e974cdcde246
pengfeiXV <877107913@qq.com>
2019-03-24 18:42:41 +0800

    添加了请假表中是否通过的标签以及排班表中是否请假的标签

--- app/model/rest.py
+++ app/model/rest.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from sqlalchemy import Column, Integer, Sequence
+from sqlalchemy import Column, Integer, Sequence, Boolean
 from app.libs.db import db
 
 
@@ -8,3 +8,4 @@ class Rest(db.Model):
     week = Column(Integer, nullable=False)
     user_id = Column(Integer, nullable=False)
     time = Column(Integer, nullable=False)  # 时间段
+    is_approval = Column(Boolean, nullable=False, default=0)
