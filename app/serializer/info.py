fb393be9470c55a4eaf4c16d17fdcba05bbe4be9
stickler-ci <support@stickler-ci.com>
2019-05-09 07:51:25 +0000

    Fixing style errors.

--- app/serializer/info.py
+++ app/serializer/info.py
@@ -2,8 +2,10 @@ from marshmallow import Schema, fields
 
 
 class InfoParaSchema(Schema):
-    department_id = fields.Integer(required=True, validate=lambda x: 0 <= x <= 10000)
-    position_id = fields.Integer(required=True, validate=lambda x: 0 <= x <= 10000)
+    department_id = fields.Integer(
+        required=True, validate=lambda x: 0 <= x <= 10000)
+    position_id = fields.Integer(
+        required=True, validate=lambda x: 0 <= x <= 10000)
     time = fields.Integer(required=True, validate=lambda x: 0 < x <= 2 ** 24)
 
 
@@ -18,7 +20,9 @@ class InfoModifyParaSchema(Schema):
     school = fields.Integer(required=True)
     experience = fields.String(required=False)
     skill = fields.String(required=False)
-    department_id = fields.Integer(required=True, validate=lambda x: 0 <= x <= 10000)
-    position_id = fields.Integer(required=True, validate=lambda x: 0 <= x <= 10000)
+    department_id = fields.Integer(
+        required=True, validate=lambda x: 0 <= x <= 10000)
+    position_id = fields.Integer(
+        required=True, validate=lambda x: 0 <= x <= 10000)
     time = fields.Integer(required=True, validate=lambda x: 0 < x <= 2 ** 24)
     username = fields.String(required=True)
